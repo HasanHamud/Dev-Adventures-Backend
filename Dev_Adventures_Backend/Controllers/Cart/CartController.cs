3bd9ff5f23e248b96b3c02082def74cf75dd5b6e
malek <rizkmalek71@gmail.com>
2025-01-10 23:02:05 +0200

    Added Cart

---
+++ Dev_Adventures_Backend/Controllers/Cart/CartController.cs
@@ -0,0 +1,84 @@
+﻿using Dev_Db.Data;
+using Dev_Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.General;
+
+namespace Dev_Adventures_Backend.Controllers.Cart
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly Dev_DbContext _context;
+        private readonly UserManager<User> _userManager;
+        public CartController(Dev_DbContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+
+
+
+
+        [Route("Course/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> AddCourse([FromRoute] int ID)
+        {
+            var UserID = _userManager.GetUserId(User);
+            var course = await _context.Courses.FirstOrDefaultAsync(s => s.Id == ID);
+            var userCart = await _context.Carts.FirstOrDefaultAsync(s => s.UserId.Equals(UserID));
+
+            if(course == null) {
+            
+            return NotFound();
+            }
+            else if(userCart.courses.Contains(course))
+            {
+                return BadRequest("Course Already in Cart");
+            }
+            else
+            {
+                userCart.courses.Add(course);
+
+               await _context.SaveChangesAsync();
+
+                return Ok("Course succesfully added");
+            }
+           
+        }
+
+
+        [Route("Course/{id}")]
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromRoute] int ID)
+        {
+            var UserID = _userManager.GetUserId(User);
+            var course = await _context.Courses.FirstOrDefaultAsync(s => s.Id == ID);
+            var userCart = await _context.Carts.FirstOrDefaultAsync(s => s.UserId.Equals(UserID));
+
+            if (course == null)
+            {
+
+                return NotFound();
+            }
+            else if (!userCart.courses.Contains(course))
+            {
+                return BadRequest("Course deoesnt exist in Cart");
+            }
+            else
+            {
+                userCart.courses.Remove(course);
+
+                await _context.SaveChangesAsync();
+
+                return Ok("Course succesfully deleted");
+            }
+
+        }
+
+    }
+}
---
+++ Dev_Models/Cart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev_Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public decimal totalPrice { get; set; } = 0;
+
+        public List<Course> courses { get; set; } = new List<Course>();
+
+
+        public Cart()
+        {
+
+        }
+
+        public Cart(string UID)
+        {
+            UserId = UID;
+        }
+
+    }
+}
